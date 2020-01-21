eb3f5761a62ab8ac6f5494171182b1eec0d98a66
sunidhihaldar <sunidhihaldar@gmail.com>
2020-01-21 10:56:00 +0530

    [sunidhi].add -> UserEntity class

---
+++ src/main/java/com/bridgelabz/fundooNotes/UserEntity.java
@@ -0,0 +1,99 @@
+package com.bridgelabz.fundooNotes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class UserEntity {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable = false, unique = true)
+	private String userId;
+	
+	@Column(nullable = false, length = 50)
+	private String firstName;
+	
+	@Column(nullable = false, length = 50)
+	private String lastName;
+	
+	@Column(nullable = false, length = 150, unique = true)
+	private String email;
+	
+	@Column(nullable = false, length = 50)
+	private long mobileNumber;
+	
+	@Column(nullable = false, length = 50, unique = true)
+	private String username;
+	
+	@Column(nullable = false)
+	private String encryptedPassword;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return encryptedPassword;
+	}
+
+	public void setPassword(String password) {
+		this.encryptedPassword = password;
+	}
+}
