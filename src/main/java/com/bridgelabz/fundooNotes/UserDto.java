8ad9cd4e61fb84c9b0bedf945f1244a3d6a905ad
sunidhihaldar <sunidhihaldar@gmail.com>
2020-01-21 11:03:40 +0530

    [sunidhi].add -> UserDto class

---
+++ src/main/java/com/bridgelabz/fundooNotes/UserDto.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.fundooNotes;
+
+//Data transfer object class
+public class UserDto {
+
+	private String userId;
+	private String firstName;
+	private  String lastName;
+	private String email;
+	private long mobileNumber;
+	private String password;
+	private String encryptedPassword;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
+}
