b5cdcc34c7572c2174234321bd33c78d9c829816
sunidhihaldar <sunidhihaldar@gmail.com>
2020-01-28 13:37:49 +0530

    [sunidhi].add -> NoteUpdation class

---
+++ src/main/java/com/bridgelabz/fundooNotes/dto/NoteUpdation.java
@@ -0,0 +1,86 @@
+package com.bridgelabz.fundooNotes.dto;
+
+import java.time.LocalDateTime;
+
+public class NoteUpdation {
+
+	private long noteId;
+
+	private String title;
+
+	private String description;
+
+	private boolean isPinned;
+
+	private boolean isArchived;
+
+	private boolean isTrashed;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	public long getNoteId() {
+		return noteId;
+	}
+
+	public void setNoteId(long noteId) {
+		this.noteId = noteId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isPinned() {
+		return isPinned;
+	}
+
+	public void setPinned(boolean isPinned) {
+		this.isPinned = isPinned;
+	}
+
+	public boolean isArchived() {
+		return isArchived;
+	}
+
+	public void setArchived(boolean isArchived) {
+		this.isArchived = isArchived;
+	}
+
+	public boolean isTrashed() {
+		return isTrashed;
+	}
+
+	public void setTrashed(boolean isTrashed) {
+		this.isTrashed = isTrashed;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
