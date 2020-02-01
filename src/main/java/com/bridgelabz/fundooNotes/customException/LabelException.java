e34d0539ffb6effa5e274756f7d3eb5c7d3cea47
sunidhihaldar <sunidhihaldar@gmail.com>
2020-02-01 11:43:38 +0530

    [sunidhi].add -> LabelException class

---
+++ src/main/java/com/bridgelabz/fundooNotes/customException/LabelException.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.fundooNotes.customException;
+
+public class LabelException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public LabelException(String message) {
+		super(message);
+	}
+}
